acce7c93ebd6c9653050bd877e68e537df28a7ab
강성구 <sungku2757@gmail.com>
2025-07-16 13:18:57 +0900

    feat: 웹 사이트 라우팅 연결하기 answer

--- src/App.jsx
+++ src/App.jsx
@@ -1,39 +1,46 @@
 import "./App.css";
 import headerStyle from "./assets/styles/header.module.css";
-/* Layout 컴포넌트
-1. 모든 <Link/> 요소의 클래스명은 "header-link" 입니다.
-2. /home으로 라우팅할 수 있는 <Link/>를 선언하세요.
-3. /about으로 라우팅할 수 있는 <Link/>를 선언하세요.
-4. /contact으로 라우팅할 수 있는 <Link/>를 선언하세요.
-5. 모든 잘못된 경로로 라우팅할 수 있는 <Link/>를 선언하세요.
-5-1. 아무 경로를 할당하거나, Math.random()을 활용할 수 있습니다.
-6. Layout 컴포넌트는 "모든" 컴포넌트에서 보여야 합니다. <Outlet/>를 추가하세요.
-*/
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Outlet,
+  Link,
+} from "react-router-dom";
+import ErrorPage from "./pages/ErrorPage";
+import AboutPage from "./pages/AboutPage";
+import ContactPage from "./pages/ContactPage";
+import HomePage from "./pages/HomePage";
 function Layout() {
   return (
     <>
       <header className={headerStyle.header}>
         <div>Router Practice</div>
         <div className={headerStyle.headerLinks}>
-          {/* 여기에 Link 컴포넌트를 할당하세요. */}
+          <Link to="/">Home</Link>
+          <Link to="/about">About</Link>
+          <Link to="/contact">Contact</Link>
+          <Link to={`/${Math.random(10)}`}>Random</Link>
         </div>
       </header>
-      {/* 이곳에 Outlet을 할당하세요. */}
+      <Outlet />
     </>
   );
 }
 
-/* App 컴포넌트
-1. 모든 라우팅을 감싸는 BrowserRouter를 선언하세요.
-2. Routes를 선언하세요.
-3. 모든 라우팅에서 보이는 Layout의 Route를 선언하세요.
-4. / 라우팅에서 보이는 <HomePage/> 컴포넌트를 라우팅에 연결하세요.
-5. /about 라우팅에서 보이는 <AboutPage/> 컴포넌트를 라우팅에 연결하세요.
-6. /contact 라우팅에서 보이는 <ContactPage/> 컴포넌트를 라우팅에 연결하세요.
-7. /* 모든 잘못된 라우팅에서 보이는 <ErrorPage/> 컴포넌트를 라우팅에 연결하세요.
-*/
 function App() {
-  return <></>;
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<HomePage />} />
+          <Route path="/about" element={<AboutPage />} />
+          <Route path="/contact" element={<ContactPage />} />
+          <Route path="*" element={<ErrorPage />} />
+        </Route>
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
