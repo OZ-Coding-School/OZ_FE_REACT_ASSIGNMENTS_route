9cd3e04a6ce59cf7eeb79a6b7c6e60afb26cc918
L1m3Kun <sungku2757@gmail.com>
2025-11-13 10:48:01 +0900

    과제 안내 주석 수정

--- src/App.jsx
+++ src/App.jsx
@@ -27,10 +27,10 @@ function Layout() {
 2. Routes를 선언하세요.
 3. 모든 라우팅에서 보이는 Layout의 Route를 선언하세요.
 4. / 라우팅에서 보이는 <HomePage/> 컴포넌트를 라우팅에 연결하세요.
-4. /home 라우팅에서 보이는 <HomePage/> 컴포넌트를 라우팅에 연결하세요.
-5. /about 라우팅에서 보이는 <AboutPage/> 컴포넌트를 라우팅에 연결하세요.
-6. /contact 라우팅에서 보이는 <ContactPage/> 컴포넌트를 라우팅에 연결하세요.
-7. /* 모든 잘못된 라우팅에서 보이는 <ErrorPage/> 컴포넌트를 라우팅에 연결하세요.
+5. /home 라우팅에서 보이는 <HomePage/> 컴포넌트를 라우팅에 연결하세요.
+6. /about 라우팅에서 보이는 <AboutPage/> 컴포넌트를 라우팅에 연결하세요.
+7. /contact 라우팅에서 보이는 <ContactPage/> 컴포넌트를 라우팅에 연결하세요.
+8. /* 모든 잘못된 라우팅에서 보이는 <ErrorPage/> 컴포넌트를 라우팅에 연결하세요.
 */
 function App() {
   return <></>;
